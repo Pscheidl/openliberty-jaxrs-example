24101b9b2e965256f3ed227befffb37b276a54fa
Pscheidl Pavel <pavel.junior@pscheidl.cz>
2017-10-25 06:12:25 +0200

    Google Guava and Apache Commons Lang dependencies

--- src/main/java/cool/pavel/openliberty/api/PingPong.java
+++ src/main/java/cool/pavel/openliberty/api/PingPong.java
@@ -1,15 +1,22 @@
 package cool.pavel.openliberty.api;
 
+import com.google.common.collect.ImmutableSet;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/ping")
 public class PingPong {
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public Response ping() {
-        return Response.ok("Pong")
+        return Response.ok(ImmutableSet.of(ImmutablePair.of("Pong", "Kong"),
+                ImmutablePair.of("Dong", "Kong")))
                 .build();
     }
 }
